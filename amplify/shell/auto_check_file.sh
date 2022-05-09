eaee36504319e0ccdf3407fe51afa13046ea2b53
peterlee <925460675@qq.com>
2022-05-09 10:24:52 +0800

    修改脚本参数

--- amplify/shell/auto_check_file.sh
+++ amplify/shell/auto_check_file.sh
@@ -1,7 +1,20 @@
 #!/usr/bin/env bash
 
 cd '..'
-cd 'python'
+cd 'python' || exit
+
+pip3_list=$(pip3 list)
+need_list=(openpyxl pandas xlrd xlwt)
+
+for need_package in "${need_list[@]}"
+do
+  if [[ $pip3_list =~  $need_package ]];then
+    echo "$need_package" is installed
+  else
+    pip3 install "$need_package"
+  fi
+done
+
 
 if [[ $1 = "check" ]];then
   python3 check_data.py
